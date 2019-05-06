dee95ead6c4986e37c784d390d8d67b481450486
Steve DeGele <sdegele@gmail.com>
2019-05-05 22:56:31 -0400

    add license and hex stuff

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Scrappy.MixProject do
   use Mix.Project
 
+  @description "Scrappy is a scraper."
+  @version "0.1.0"
   def project do
     [
       app: :scrappy,
+      name: "Scrappy",
+      version: @version,
+      description: @description,
       version: "0.1.0",
       elixir: "~> 1.8",
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/arghmeleg/scrappy"
     ]
   end
 
@@ -22,5 +29,22 @@ defmodule Scrappy.MixProject do
       {:httpoison, "1.5.0"},
       {:floki, "~> 0.20.4"}
     ]
+  end
+
+  defp package do
+    %{
+      maintainers: ["Steve DeGele"],
+      licenses: ["MIT"],
+      files: [
+        "lib",
+        "test",
+        "mix.exs",
+        "README.md",
+        "LICENSE",
+      ],
+      links: %{
+        "GitHub" => "https://github.com/arghmeleg/scrappy"
+      }
+    }
   end
 end
